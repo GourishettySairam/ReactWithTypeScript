41ad4a16f2d0570ea94dc0e1845a4cae77aeb6d8
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2021-07-18 00:24:59 +0530

    setting up CSS Modules

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import pizzas from '../data/pizzas.json';
+import Pizza from './Pizza';
+import AppCSS from './App.module.css';
+
+
+const App = () => {    
+    return (
+    <div className={AppCSS.container}>
+        <ul>
+            {pizzas.map((pizza) => {
+                    return <Pizza key={pizza.id} pizza={pizza} />
+            })}
+        </ul>
+    </div>)
+};
+
+export default App;
